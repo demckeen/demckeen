f475b0fdea7fa2c8def913e6680b6c617feef215
zircadia <76924125+zircadia@users.noreply.github.com>
2021-10-16 13:57:10 -0500

    todolist efforts

--- challenge1/js/ls.js
+++ challenge1/js/ls.js
@@ -2,10 +2,15 @@
 // the value is stored under in LS
 // @return {array} The value as an array of objects
 
-function readFromLS(key) { }
+function readFromLS(toDos) {
+    toDoList = JSON.parse(window.localStorage.getItem(toDos));
+    return toDoList;
+ }
 
 // write an array of objects to local storage under the provided key @param {string} key The key 
 // under which the value is stored under in LS
 // * @param {array} data The information to be stored as an array of objects. Must be serialized.
 
-function writeToLS(key, data) { }
+function writeToLS(toDos, toDoList) {
+    window.localStorage.setItem(toDos, JSON.stringify(toDoList));
+ }
