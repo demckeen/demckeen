153e422b04bda4ac895ddf68c3a4725ea525bdc4
zircadia <76924125+zircadia@users.noreply.github.com>
2021-10-21 21:33:26 -0500

    commit dos

--- challenge1/js/ls.js
+++ challenge1/js/ls.js
@@ -1,9 +1,13 @@
+export {readFromLS, writeToLS, toDoList, removeTask, updateTask};
+
 // read a value from local storage and parse it as JSON @param {string} key The key under which 
 // the value is stored under in LS
 // @return {array} The value as an array of objects
 
-function readFromLS(toDos) {
-    toDoList = JSON.parse(window.localStorage.getItem(toDos));
+let toDoList = [];
+
+function readFromLS(toDoList) {
+    toDoList = JSON.parse(window.localStorage.getItem(toDoList))
     return toDoList;
  }
 
@@ -11,6 +15,21 @@ function readFromLS(toDos) {
 // under which the value is stored under in LS
 // * @param {array} data The information to be stored as an array of objects. Must be serialized.
 
-function writeToLS(toDos, toDoList) {
-    window.localStorage.setItem(toDos, JSON.stringify(toDoList));
+function writeToLS(task, toDoList) { 
+    toDoList.push(task);
+    localStorage.setItem("toDoList", JSON.stringify(toDoList));
  }
+
+function removeTask(taskId) {
+    toDoList = toDoList.filter(element => String(element.id) !== taskId);
+    localStorage.setItem("toDoList", JSON.stringify(toDoList));
+    document.getElementById(taskId).remove();
+
+  }
+
+function updateTask(taskId, status) {
+    toDoList = toDoList.map(item => {if (String(item.id) === taskId)
+    {return {...item, completed: status}}
+    else {return item}});
+    localStorage.setItem("toDoList", JSON.stringify(toDoList));
+}
--- challenge1/js/utilities.js
+++ challenge1/js/utilities.js
@@ -1,3 +1,5 @@
+export {qs, onTouch, renderTodoList, renderCompletedList, renderActiveList};
+
 // do a querySelector lookup @param {string} selector The selector passed to querySelector
 // @return {element} The matching element or null if not found /
 
@@ -35,4 +37,78 @@ function onTouch(elementSelector, callback) {
 our list elements into.
 */
 
-function renderTodoList(list, element) {}
+function renderTodoList(list, element) {
+    element.innerHTML = '';
+    for (let i = 0; i < list.length; i++) {
+        let item = document.createElement("li");
+        let checkbox = document.createElement("span");
+        let remove = document.createElement("span");
+        let text = list[i].content;
+        let id = list[i].id;
+
+        checkbox.classList.add("checkbox");
+        remove.classList.add("delete");
+        item.classList.add("task");
+        item.setAttribute('id', id);
+
+        if (list[i].completed === true) {
+        checkbox.classList.add("complete");
+        item.classList.add("completeli");
+        }
+
+        item.textContent = text;
+        item.appendChild(checkbox);
+        item.appendChild(remove);
+
+        element.appendChild(item);   
+        
+    }}
+
+    function renderCompletedList(list, element) {
+        element.innerHTML = '';
+        for (let i = 0; i < list.length; i++) {
+            let item = document.createElement("li");
+            let checkbox = document.createElement("span");
+            let remove = document.createElement("span");
+            let text = list[i].content;
+            let id = list[i].id;
+    
+            checkbox.classList.add("checkbox");
+            checkbox.classList.add("complete");
+            remove.classList.add("delete");
+            item.classList.add("task");
+            item.classList.add("completeli");
+            item.setAttribute('id', id);
+    
+            item.textContent = text;
+            item.appendChild(checkbox);
+            item.appendChild(remove);
+    
+            element.appendChild(item);   
+            
+        }}
+
+        function renderActiveList(list, element) {
+            element.innerHTML = '';
+            for (let i = 0; i < list.length; i++) {
+                let item = document.createElement("li");
+                let checkbox = document.createElement("span");
+                let remove = document.createElement("span");
+                let text = list[i].content;
+                let id = list[i].id;
+        
+                checkbox.classList.add("checkbox");
+                remove.classList.add("delete");
+                item.classList.add("task");
+                item.setAttribute('id', id);
+        
+                item.textContent = text;
+                item.appendChild(checkbox);
+                item.appendChild(remove);
+        
+                element.appendChild(item);   
+                
+            }}
+
+
+
