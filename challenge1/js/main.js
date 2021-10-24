333bb0bad14bd889bce01ed6e2defe69452f79d1
zircadia <76924125+zircadia@users.noreply.github.com>
2021-10-24 09:44:55 -0500

    update

--- challenge1/js/main.js
+++ challenge1/js/main.js
@@ -6,20 +6,25 @@ const toDos = new ToDos;
 
 getTodos();
 
-let button = document.getElementById('addNewTask');
+window.addEventListener("keydown",checkWord);  
+function checkWord(event){
+  console.log(event.keyCode);
+}
+
+var button = document.getElementById('addNewTask');
 
 document.querySelector("#addTask").addEventListener("submit", function(e){
       e.preventDefault();
       addTask();    //stop form from submitting
   });
 
-// button.addEventListener('click', addTask);
-// button.addEventListener('touchend', addTask);
-// document.getElementById('newTask').addEventListener('keypress', function (e) {
-//     if (e.key === 'Enter') {
-//       addTask();
-//     }
-// });
+button.addEventListener('click', addTask);
+button.addEventListener('touchend', addTask);
+document.getElementById('newTask').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      addTask();
+    }
+});
 
 function addTask() {
     toDos.addTodo();
